ca345bbba1da1a5aae350dbe8e26fa9d947b0e43
Duhless-dev <nikita1998fryizino@inbox.ru>
2020-06-13 02:31:05 +0300

    add product-det fix mixins

--- app/js/main.js
+++ app/js/main.js
@@ -79,5 +79,15 @@ $(function () {
 	$('.burger__btn').on('click', function () {
 		$('.header__down').slideToggle();
 	});
+
+	$('.wrapper-tabs .tab').on('click', function (event) {
+		var id = $(this).attr('data-id');
+		$('.wrapper-tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.wrapper-tabs .tabs').find('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('#' + id).addClass('active-tab').fadeIn();
+		return false;
+	});
+
 	var mixer = mixitup('.product__inner-filter-item');
 });
